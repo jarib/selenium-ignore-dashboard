52cff2d38853f3e68d36b492bddf1d630dc387b8
Jari Bakken <jari.bakken@gmail.com>
2011-10-28 23:09:16 -0700

    Vertically center spinner.

--- public/js/src/app.js
+++ public/js/src/app.js
@@ -2,36 +2,40 @@ var app = Sammy('#main', function() {
   this.use('Mustache');
 
   this.get('#/ignores', function() {
-    this.swap('<div class="spinner"></div>')
-    this.load('ignores.json')
+    this.render("spinner.mustache")
+        .swap()
+        .load('ignores.json')
         .render('ignores.mustache')
         .then(function() { Menu.selectLink("list-link"); })
         .swap();
   });
 
   this.get("#/ignores/:driver", function() {
-    this.swap('<div class="spinner"></div>')
-    this.load('ignores/' + this.params.driver + ".json")
+    this.render("spinner.mustache")
+        .swap()
+        .load('ignores/' + this.params.driver + ".json")
         .render('ignores.mustache')
         .then(function() { Menu.selectLink("list-link"); })
         .swap();
   })
 
   this.get("#/stats", function(context) {
-    this.load("stats.json").then(function(data) {
-      context.render("stats.mustache", data)
-             .swap()
-             .then(function() { Menu.selectLink("stats-link"); })
-             .then(function() {
-               var stats = new Stats(data);
-               var line = document.getElementById('line-graph');
-               var column = document.getElementById("column-graph")
+    this.render("spinner.mustache")
+        .swap()
+        .load("stats.json").then(function(data) {
+          context.render("stats.mustache", data)
+                 .swap()
+                 .then(function() { Menu.selectLink("stats-link"); })
+                 .then(function() {
+                   var stats = new Stats(data);
+                   var line = document.getElementById('line-graph');
+                   var column = document.getElementById("column-graph")
 
-               stats.renderLineGraphTo(line);
-               stats.renderColumnGraphTo(column);
-             });
-    });
-  })
+                   stats.renderLineGraphTo(line);
+                   stats.renderColumnGraphTo(column);
+                 });
+        });
+    })
 });
 
 app.run('#/ignores');
