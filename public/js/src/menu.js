2bbbc58d7519c95a45dbd206346fce293b85c6aa
Jari Bakken <jari.bakken@gmail.com>
2011-10-29 13:01:55 -0700

    Remove a log statement.

--- public/js/src/menu.js
+++ public/js/src/menu.js
@@ -2,7 +2,6 @@ var Menu = (function() {
   return {
     selectLink: function(id) {
       $(".menu a").each(function(index) {
-        console.log(this.id);
         if (this.id == id) {
           $(this).addClass("selected");
         } else {
