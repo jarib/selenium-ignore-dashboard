2b34ee970c6ae025adf443134826d77a33ad9eb4
Jari Bakken <jari.bakken@gmail.com>
2011-10-29 17:25:11 -0700

    Diable diff selects while loading.

--- public/js/src/diff.js
+++ public/js/src/diff.js
@@ -5,14 +5,17 @@ var Diff = (function() {
     init: function(ctx) {
       context = ctx;
       $("select").change(function() {
+
         var drivers = $("select").map(function() { return this.value; });
         context.redirect("#/diff/" + drivers.toArray().join(","));
       });
     },
-    
+
     loadDrivers: function(drivers) {
+      $("select").attr('disabled', true);
+
       var path = "diff/" + drivers.join(',') + ".json";
-      
+
       $(drivers).each(function(index) {
         $('select').get(index).value = this;
       });
@@ -24,6 +27,8 @@ var Diff = (function() {
                 ignores: data,
                 count: data.length
                }).replace('#diff');
+
+               $("select").attr('disabled', false);
              })
       ;
     }
